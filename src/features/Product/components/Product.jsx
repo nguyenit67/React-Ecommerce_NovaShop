199ddd3824722058389e9cb004184a8cd2d34a38
nguyenit67 <iamnghoangnguyen@gmail.com>
2021-09-11 21:34:53 +0700

    hiển thị list product (dạng raw trong skeleton list)

--- src/features/Product/components/Product.jsx
+++ src/features/Product/components/Product.jsx
@@ -11,7 +11,10 @@ function Product({ product }) {
   return (
     <Box padding={1}>
       <Skeleton variant="rect" width="100%" height={118} />
-      <Typography variant="body2"></Typography>
+      <Typography variant="body2">{product.name} </Typography>
+      <Typography variant="body2">
+        {product.salePrice} -{product.promotionPercent}%
+      </Typography>
     </Box>
   );
 }
