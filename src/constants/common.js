9c5ef58071d2b753d38ff32d1f3504bb07b7402a
nguyenit67 <iamnghoangnguyen@gmail.com>
2021-09-17 13:34:00 +0700

    Xử lý thumbnail cho sản phẩm

---
+++ src/constants/common.js
@@ -0,0 +1,2 @@
+export const STATIC_HOST = 'https://api.ezfrontend.com';
+export const THUMBNAIL_PLACEHOLDER = 'https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-product-3_large.png?format=webp&v=1530129341';
