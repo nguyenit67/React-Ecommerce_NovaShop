3e19072e89ef0331ee22df819c630b487a8dea7d
nguyenit67 <iamnghoangnguyen@gmail.com>
2021-09-28 19:18:27 +0700

    Làm chức năng Pagination

--- src/App.js
+++ src/App.js
@@ -8,18 +8,6 @@ import CounterFeature from './features/Counter';
 import TodoFeature from './features/Todo';
 
 function App() {
-  // useEffect(() => {
-  //   const fetchProducts = async () => {
-  //     const params = {
-  //       _limit: 10,
-  //     };
-  //     const productList = await productApi.getAll(params);
-  //     // console.log(productList);
-  //   };
-
-  //   fetchProducts();
-  // }, []);
-
   return (
     <div className="app">
       <Header />
--- src/features/Product/pages/ListPage.jsx
+++ src/features/Product/pages/ListPage.jsx
@@ -1,4 +1,5 @@
 import { Box, Container, Grid, makeStyles, Paper } from '@material-ui/core';
+import { Pagination } from '@material-ui/lab';
 import productApi from 'api/productApi';
 import React, { useEffect, useState } from 'react';
 import ProductList from '../components/ProductList';
@@ -17,20 +18,35 @@ const useStyles = makeStyles((theme) => ({
 function ListPage(props) {
   const classes = useStyles();
   const [productList, setProductList] = useState([]);
+  const [pagination, setPagination] = useState({
+    page: 1,
+    limit: 9,
+    total: 10,
+  });
   const [loading, setLoading] = useState(true);
+  const [filters, setFilters] = useState({ _page: 1, _limit: 9 });
 
   useEffect(() => {
     (async () => {
       try {
-        const { data } = await productApi.getAll({ _page: 1, _limit: 20 });
+        const { data, pagination } = await productApi.getAll(filters);
         setProductList(data);
+        setPagination(pagination);
+        console.log({ data, pagination });
       } catch (error) {
         console.log('Fail to fetch product list', { error });
       }
 
       setLoading(false);
     })();
-  }, []);
+  }, [filters]);
+
+  const handlePaginationChange = (e, page) => {
+    setFilters((prevFilters) => ({
+      ...prevFilters,
+      _page: page,
+    }));
+  };
 
   /* 
     section.product-list-page 
@@ -49,10 +65,17 @@ function ListPage(props) {
           <Grid item className={classes.right}>
             <Paper elevation={0}>
               {loading ? (
-                <ProductSkeletonList />
+                <ProductSkeletonList length={9} />
               ) : (
                 <ProductList data={productList} />
               )}
+
+              <Pagination
+                color="primary"
+                count={Math.ceil(pagination.total / pagination.limit)}
+                page={pagination.page}
+                onChange={handlePaginationChange}
+              />
             </Paper>
           </Grid>
         </Grid>
