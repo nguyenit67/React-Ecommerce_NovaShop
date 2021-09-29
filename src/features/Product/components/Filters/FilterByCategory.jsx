aa4a05265d9fb92fdab61e5711b4b33c76da2252
nguyenit67 <iamnghoangnguyen@gmail.com>
2021-09-29 20:08:01 +0700

    132. Giao diện lọc danh mục sản phẩm

--- src/features/Product/components/Filters/FilterByCategory.jsx
+++ src/features/Product/components/Filters/FilterByCategory.jsx
@@ -1,7 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import { Box, Typography } from '@material-ui/core';
+import { Box, makeStyles, Typography } from '@material-ui/core';
 import categoryApi from 'api/categoryApi';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+  menu: {
+    padding: 0,
+    margin: 0,
+    listStyleType: 'none',
+
+    '& > li': {
+      marginTop: theme.spacing(1),
+      transition: 'all .25s',
+      cursor: 'pointer',
+
+      '&:hover': {
+        color: theme.palette.primary.dark,
+      },
+    },
+  },
+}));
 
 FilterByCategory.propTypes = {
   onChange: PropTypes.func,
@@ -9,6 +30,7 @@ FilterByCategory.propTypes = {
 
 function FilterByCategory({ onChange }) {
   const [categoryList, setCategoryList] = useState([]);
+  const classes = useStyles();
 
   useEffect(() => {
     (async () => {
@@ -33,12 +55,13 @@ function FilterByCategory({ onChange }) {
   };
 
   return (
-    <Box>
-      <Typography>DANH MỤC SẢN PHẨM</Typography>
-      <ul>
+    <Box className={classes.root}>
+      <Typography variant="subtitle2">DANH MỤC SẢN PHẨM</Typography>
+
+      <ul className={classes.menu}>
         {categoryList.map((category) => (
           <li key={category.id} onClick={() => handleCategoryClick(category)}>
-            {category.name}
+            <Typography variant="body2">{category.name}</Typography>
           </li>
         ))}
       </ul>
