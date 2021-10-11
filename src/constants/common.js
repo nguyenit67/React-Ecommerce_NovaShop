c001cf8999ce0727c4e6ebae05ab7c831351572e
nguyenit67 <iamnghoangnguyen@gmail.com>
2021-10-11 21:10:41 +0700

    change product placeholder

--- src/constants/common.js
+++ src/constants/common.js
@@ -1,2 +1,3 @@
 export const STATIC_HOST = 'https://api.ezfrontend.com';
-export const THUMBNAIL_PLACEHOLDER = 'https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-product-3_large.png?format=webp&v=1530129341';
+export const THUMBNAIL_PLACEHOLDER =
+  'https://ecohealthinstitute.websites.co.in/dummytemplate/img/product-placeholder.png';
