8f2e2d2c715778a25afa9e555c73899be1d059c1
nguyenit67 <iamnghoangnguyen@gmail.com>
2021-10-13 10:07:14 +0700

    add currency formatters

---
+++ src/utils/unitFormatters.js
@@ -0,0 +1,58 @@
+export function formatCurrencyWithoutSign(
+  number = 0,
+  { locale = 'vi-VN', currency = 'VND' } = {}
+) {
+  return new Intl.NumberFormat(locale, {
+    style: 'currency',
+    currency: currency,
+    currencyDisplay: 'code',
+  })
+    .format(number)
+    .replace(currency, '')
+    .trim();
+}
+
+export function formatCurrency(
+  number = 0,
+  { locale = 'vi-VN', currency = 'VND', displayType = 'default' } = {}
+) {
+  // let result = null;
+
+  switch (displayType) {
+    case 'shortVND': {
+      const formattedNumber = formatCurrencyWithoutSign(number, {
+        locale,
+        currency,
+      });
+      const result = `${formattedNumber}đ`;
+      return result;
+    }
+
+    default: {
+      const defaultFormatter = new Intl.NumberFormat(locale, {
+        style: 'currency',
+        currency: currency,
+      });
+      const result = defaultFormatter.format(number);
+      return result;
+    }
+  }
+}
+
+export function formatPrice(
+  number = 0,
+  { locale = 'vi-VN', currency = 'VND' } = {}
+) {
+  return formatCurrency(number, { locale, currency });
+}
+
+export function formatPriceShort(
+  number = 0,
+  { locale = 'vi-VN', currency = 'VND' } = {}
+) {
+  return formatCurrency(number, {
+    locale,
+    currency,
+    displayType: 'shortVND',
+  });
+}
