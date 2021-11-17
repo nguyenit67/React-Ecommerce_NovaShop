a2526a617516a1489c2d9ba5b09a4f6494ed1903
nguyenit67 <iamnghoangnguyen@gmail.com>
2021-11-17 17:02:51 +0700

    143. Setup layout cho trang chi tiết sản phẩm

--- src/features/Product/index.jsx
+++ src/features/Product/index.jsx
@@ -2,15 +2,17 @@ import { Box } from '@material-ui/core';
 import ListPage from 'features/Product/pages/ListPage';
 import React from 'react';
 import { Route, Switch, useRouteMatch } from 'react-router-dom';
+import DetailPage from './pages/DetailPage';
 
 function ProductFeature(props) {
   const match = useRouteMatch();
+  console.log('NLogging `match`', match);
 
   return (
     <Box pt={4}>
       <Switch>
         <Route path={match.path} exact component={ListPage} />
-        {/* <Route path={match.path} component={ProductFeature} /> */}
+        <Route path={`${match.path}/:productId`} component={DetailPage} />
       </Switch>
     </Box>
   );
