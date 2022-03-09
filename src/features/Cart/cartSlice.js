96ebe0dcaf7fc9e703a839fc2994bae50894a554
nguyenit67 <iamnghoangnguyen@gmail.com>
2022-03-09 11:11:50 +0700

    156. Tạo actions cho cart items

--- src/features/Cart/cartSlice.js
+++ src/features/Cart/cartSlice.js
@@ -4,18 +4,43 @@ const cartSlice = createSlice({
   name: 'cart',
   initialState: {
     showMiniCart: false,
+    cartItems: [],
   },
   reducers: {
-    showMiniCart: (state) => {
+    showMiniCart(state) {
       state.showMiniCart = true;
     },
-    hideMiniCart: (state) => {
+    hideMiniCart(state) {
       state.showMiniCart = false;
+    },
+
+    addToCart(state, action) {
+      // newItem = {id, product, quantity}
+      const newItem = action.payload;
+      const existItem = state.cartItems.find((item) => item.id === newItem.id);
+      if (existItem) {
+        existItem.quantity += newItem.quantity;
+      } else {
+        state.cartItems.push(newItem);
+      }
+    },
+
+    setQuantity(state, action) {
+      const { id, quantity } = action.payload;
+      const itemToUpdate = state.cartItems.find((item) => item.id === id);
+      if (itemToUpdate) {
+        itemToUpdate.quantity = quantity;
+      }
+    },
+
+    removeFromCart(state, action) {
+      const idNeedToRemove = action.payload;
+      state.cartItems = state.cartItems.filter((item) => item.id !== idNeedToRemove);
     },
   },
 });
 
 const { actions, reducer: cartReducer } = cartSlice;
-export const { showMiniCart, hideMiniCart } = actions;
+export const { showMiniCart, hideMiniCart, addToCart, setQuantity, removeFromCart } = actions;
 
 export default cartReducer;
--- src/features/Cart/index.jsx
+++ src/features/Cart/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { addToCart } from './cartSlice';
 
 CartFeature.propTypes = {};
 
 function CartFeature(props) {
+  const action = addToCart();
+  console.log(action);
+
   return <div>Cart Feature</div>;
 }
 
